49edddce55c115386cfe218ec74e9e1fead546cc
kai-0307 <93191804+kai-0307@users.noreply.github.com>
2023-05-22 12:01:03 +0900

    "first commit"

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,37 @@ struct Todo {
     map: HashMap<String, bool>,
 }
 
+// insertメソッドをTodo構造体に定義
 impl Todo {
     fn insert(&mut self, key: String) {
-      self.map.insert(key, true);
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content);
+        std::fs::write("./data/db.txt", content)
+    }
+
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut list = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("./data/db.txt")?;
+        let mut content = String::new();
+        list.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()  // 文字列の各行に対するイテレータを作成
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo {map})
     }
 }
 
@@ -21,5 +49,17 @@ fn main() {
     });
 
     println!("{:?}, {:?}", action, item);
-}
 
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
+
+}
